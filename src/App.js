1075125ed231aff5a464927ef3f0dc06ad1c8cba
saderdesigner <57695878+saderdesigner@users.noreply.github.com>
2021-02-22 23:19:11 +0700

    04. withRouter High Order Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
+const Hats = () => {
+  return (
+    <div>
+      <h1>This is hats page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/hats" component={Hats} />
+    </Switch>
   );
 }
 
